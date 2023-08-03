b68da9f8b13c38fb2590bc6118d3a87b421f9483
hridayroy19 <hrhridoyroy503@gmail.com>
2023-08-03 13:07:57 +0600

    add a propartice

---
+++ porpartice.js
@@ -0,0 +1,61 @@
+
+// jodi tumi sdu kay gula dekhte chao valuaer bade tale
+
+// var shopingJCard = {
+//     pen   : 5,
+//     book  : 20,
+//     shert : 5,
+//     mouse : 2,
+//     genji : 5
+// }
+// var propartice = Object.keys(shopingJCard);
+// console.log(propartice);
+
+// console.log(shopingJCard);
+
+
+// show only value
+
+// var shopingJCard = {
+//     pen   : 5,
+//     book  : 20,
+//     shert : 5,
+//     mouse : 2,
+//     genji : 5
+// }
+// var propartice = Object.values(shopingJCard);
+// console.log(propartice);
+
+// console.log(shopingJCard);
+
+
+
+// var shopingJCard = {
+//     pen   : 5,
+//     book  : 20,
+//     shert : 5,
+//     mouse : 2,
+//     genji : 5
+// }
+// var propartice = Object.values(shopingJCard);
+// // console.log(propartice);
+
+// // console.log(shopingJCard);
+// var proparticeName ='pen';
+// var proparticevalue = shopingJCard[proparticeName];
+// console.log(proparticevalue);
+
+
+
+var shopingJCard = {
+    pen   : 5,
+    book  : 20,
+    shert : 5,
+    mouse : 2,
+    genji : 5
+}
+
+shopingJCard.mouse=50;
+
+console.log(shopingJCard);
+
