d5e7f7ef0d8918e3e32a178f09dbb69e16ec4cf3
hridayroy19 <hrhridoyroy503@gmail.com>
2023-08-05 01:58:05 +0600

    add variable fucton

---
+++ short.js
@@ -0,0 +1,26 @@
+// var num =5;
+// num +=5;
+
+// var num = 50;
+// num %= 6; 
+
+
+// var num = 5;
+// num **= 4;
+// console.log(num);
+
+
+
+// var num = '50';
+// var num2 = 20;
+
+// num = parseFloat(num);
+// sum = num + num2
+ 
+// console.log(sum);
+
+const fruits = ["apple", "banana", "orange", "grape"];
+
+const numberOfFruits = fruits.length;
+
+console.log(numberOfFruits); 
